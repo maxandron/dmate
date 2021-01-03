b36ec6516e5a4df5d7993f03d8d1206892d6e767
Jonathan-Dart-Mac <zuntah@gmail.com>
2021-01-03 19:13:04 +0800

    Get all message to object

--- datepal.js
+++ datepal.js
@@ -13,6 +13,51 @@ function isLoaded() {
     return conversation_block != 'undefined' && conversation_block != null;
 }
 
+function getMessages(whoSent){
+  if(whoSent == 'match'){
+    baseXpath = "//div[contains(@class, 'msgWrp') and contains(@class, 'Pstart(62px)')][divIndex]/div[1]"
+  }
+  if(whoSent == 'me'){
+    baseXpath = "//div[contains(@class, 'msgWrp') and contains(@class, 'Pstart(100px)')][divIndex]/div[1]"
+  }
+  if(whoSent == 'all'){
+    baseXpath = "//div[contains(@class, 'msgWrp')][divIndex]/div[1]"
+  }
+  
+  var currentMessageBlock = true;
+  var index = 1;
+  var matchMessages = [];
+
+  while(currentMessageBlock){
+    currentXpath = baseXpath.replace("divIndex", index)
+    currentMessageBlock = getElementByXpath(currentXpath);
+    if(currentMessageBlock){
+      currentMessage = getElementByXpath(currentXpath + "/div[3]/span[1]");
+      if(currentMessage){
+        matchMessages.push(currentMessage.innerHTML);
+      }
+    }
+    index += 1
+  }
+
+  return matchMessages;
+}
+
+function orderMessages(myMessages, allMessages){
+  var orderedMessages = []
+
+  allMessages.forEach(function(entry) {
+    
+    if(myMessages.includes(entry)){
+      orderedMessages.push({"Me": entry});
+    } else{
+      orderedMessages.push({"Match": entry});
+    }
+  });
+  
+  return orderedMessages;
+}
+
 var waitForEl = function(callback) {
   if (isLoaded()) {
     console.log('loaded!');
@@ -33,5 +78,15 @@ waitForEl(function() {
         datepal.innerHTML = getWidget();
         let conversation_block = getElementByXpath(conversation_block_xpath);
         insertAfter(datepal, conversation_block);
+
+        setTimeout(function() {
+          var myMessages = getMessages("me");
+          // var matchMessages = getMessages("match");
+          var allMessages = getMessages("all");
+          var orderedMessages = orderMessages(myMessages, allMessages);
+          console.log(orderedMessages);
+        }, 2000);
+        
     }
 });
+
