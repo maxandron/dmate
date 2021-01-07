39644bc169366c30874f61e3874115e3fddfac26
Ron Suhodrev <ron.suhodrev@gmail.com>
2021-01-07 23:20:33 +0800

    Fix when the message listener is loaded

--- extension/background.js
+++ extension/background.js
@@ -1,20 +1,16 @@
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({apikey: 'undefined'}, function () {
-        console.log("api key set to nothing");
-    });
-    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        console.log('got message');
-        fetch(DATEPAL_BACKEND_URL, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: request.data
-        })
-            .then(response => response.json())
-            .then(response => sendResponse(response))
-            .catch(error => console.log('Error:', error));
-        return true;
-    });
+// Listen for requests to forward to backend
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+    console.log('got message');
+    fetch(DATEPAL_BACKEND_URL, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: request.data
+    })
+        .then(response => response.json())
+        .then(response => sendResponse(response))
+        .catch(error => console.log('Error:', error));
+    return true;
 });
 
