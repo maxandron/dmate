f8de966d72b28b507e28204ac9978b5ac7d25275
Jonathan Shahak <zuntah@gmail.com>
2021-02-02 23:43:04 +0800

    Add security code, Finish security tests

--- dmate-api/tests/test_lambda_function.py
+++ dmate-api/tests/test_lambda_function.py
@@ -1,35 +1,19 @@
 import pytest
-from security import (BodyNotJsonError, MaliciousInterestError,
-                      MaliciousMatchNameError, MaliciousMessagesError,
-                      NoBodyError, NoMatchNameError, NoMessagesError)
+import requests
+from dmate.security import (
+    BodyNotJsonError,
+    MaliciousInterestError,
+    MaliciousMatchNameError,
+    MaliciousMessagesError,
+    NoBodyError,
+    NoMatchNameError,
+    NoMessagesError,
+    RecaptchaNotSentError,
+    NoUserAgentError,
+    secure_the_messages,
+)
 
 import lambda_function
-
-# def test_fetch_suggestions():
-#     # Arange
-#     USER_NAME = "User"
-#     MATCH_NAME = "Nadz"
-#     MATCH_INTERESTS = []
-#     MESSAGES = [
-#         [USER_NAME, "hi"],
-#         [MATCH_NAME, "hi"],
-#         [USER_NAME, "You're cute!"],
-#     ]
-
-
-#     # Act
-#     attributes = lambda_function.PromptAttributes(
-#         user_name="Jonathan", match_name=MATCH_NAME, interests=MATCH_INTERESTS
-#     )
-#     suggestions = lambda_function.fetch_suggestions(
-#         attributes=attributes, messages=MESSAGES
-#     )
-#     print(suggestions)
-
-#     # Assert
-#     # assert "description" in prompt
-
-#     # Cleanup
 
 
 def test_event_without_body():
@@ -44,43 +28,107 @@ def test_body_not_json():
         lambda_function.lambda_handler(event, None)
 
 
-def test_no_messages():
-    event = {"body": "{}"}
+@pytest.fixture
+def ignore_captcha_requests_response(monkeypatch):
+    class MockResponse:
+        def json(self):
+            return {"success": True, "score": 0.6}
+
+    def mock_post(*args, **kwargs):
+        return MockResponse()
+
+    monkeypatch.setattr(requests, "post", mock_post)
+
+
+def test_no_messages(ignore_captcha_requests_response):
+    event = {
+        "body": '{"recaptcha_token": "123"}',
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
+    }
+
     with pytest.raises(NoMessagesError):
         lambda_function.lambda_handler(event, None)
 
 
-def test_no_match_name():
-    event = {"body": '{"messages": "1"}'}
+def test_no_match_name(ignore_captcha_requests_response):
+    event = {
+        "body": '{"messages": [], "recaptcha_token": "123"}',
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
+    }
+
     with pytest.raises(NoMatchNameError):
         lambda_function.lambda_handler(event, None)
 
 
-def test_malicious_match_name():
-    event = {"body": '{"messages": "1", "match_name": "asdad\\n"}'}
-    with pytest.raises(MaliciousMatchNameError):
-        lambda_function.lambda_handler(event, None)
-
-
-def test_malicious_interest():
-    event = {"body": '{"messages": "1", "match_name": "asdad\\n"}'}
-    with pytest.raises(MaliciousMatchNameError):
-        lambda_function.lambda_handler(event, None)
-
-
-def test_malicious_messages():
+def test_malicious_match_name(ignore_captcha_requests_response):
     event = {
-        "body": '{"messages": "1", "match_name": "asdad", "match_interests": ["kaka"]}'
+        "body": '{"messages": [], "match_name": "asad\\n", "recaptcha_token": "123"}',
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
+    }
+    with pytest.raises(MaliciousMatchNameError):
+        lambda_function.lambda_handler(event, None)
+
+
+def test_malicious_interest(ignore_captcha_requests_response):
+    event = {
+        "body": '{"messages": [], "interests": ["as\\n", "as"], "match_name": "asad", "recaptcha_token": "123"}',
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
+    }
+    with pytest.raises(MaliciousInterestError):
+        lambda_function.lambda_handler(event, None)
+
+
+def test_malicious_messages(ignore_captcha_requests_response):
+    event = {
+        "body": '{"messages": "asdads", "interests": ["asn", "as"], "match_name": "asad", "recaptcha_token": "123"}',
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
     }
     with pytest.raises(MaliciousMessagesError):
         lambda_function.lambda_handler(event, None)
 
 
-# TODO: Continue security test
-# def test_malicious_interests():
-#     event = {"body": '{"messages": "[["1", "2"]]", "match_name": "asdad", "match_interests": ["kaka\n"]}'}
-#     with pytest.raises(MaliciousInterestError):
-#         lambda_function.lambda_handler(event, None)
+def test_no_captcha():
+    event = {
+        "body": "{}",
+        "requestContext": {
+            "http": {
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
+            }
+        },
+    }
+    with pytest.raises(RecaptchaNotSentError):
+        lambda_function.lambda_handler(event, None)
 
-# def test_legit_payload():
-#     event = {"body": {"messages":[["Match","Hey. How is it going"],["User","Pretty well actually, you? :)"],["Match","Haha. Increasingly going mad."],["Match","With work"],["Match","😂"],["User","Oh, I know what you mean. Hahaha..."],["User","What do you do?"],["Match","Lawyer"],["Match","Yourself??"],["User","Interesting 🧐"],["User","I doing cyber security education for the military 🤫"],["Match","Hahaha. The real question is who’s military"],["Match","🙊"],["User","The Singaporean military 👊"],["Match","Hahaha"],["Match","I suppose they do need to be educated"],["Match","🤪"],["User","Indeed they do :) "],["User","It's pretty cool to be able to help them improve their skills. And trust me when I say they are improving!"],["Match","I bet."],["Match","So how long are you here for?"],["User","I'm in Singapore for a year already! 😄"],["User","It is going really well..."],["User","What about you?"],["Match","Almost 3 years home I guess. All very unexpected"],["Match","This is crazy, but do you want to catch dinner this evening? Around 2030"],["User","Sorry, I just saw your message, I really appreciate you being spontaneous! &lt;3"],["User","Hopefully, we can do it another day"],["User","Maybe in the upcoming weekend? "],["Match","Next week?"],["User","Haha your weekend is packed?"],["User","Haha your weekend is packed?"],["User","Actually mine as well"],["User","Next wee ksounds awesome"],["Match","Brilliant. I’m on 9649 2235"],["Match","Drop me a whatsapp"]],"match_name":"Ezann","match_interests":["Working out","Dog lover","Grab a drink","Reading","Wine"],"version":"0.0.1","recaptcha_token":"03AGdBq26rmq4NbScxMTt9X_foui4bzAi4TW-UixEgzQuPuw556SNJ5oLD-Gw3AxLrJcTV6iHOTrt9ALsDeQDM9yI2bZaNb9LZsW_aT3JPkzLghlTrZBuoiqZATt3GBiPm9DzrEVD70OIPZXhFkc4_QfBGcreW2earOYX8Ij2H3DRsc3pE1SZYOB-KKTOZ14ePt0K5qU1taZ6af845FZvdZpXQlpNOxlRHfrxUfzWzVNKdVJaLN8h2G6YEmpG-Nm3eholUNciK8u5vrWXl5Z_65ubuup5uKu6vZk4reRdOVQ-5bf2wRrE09j8_gKkmUM-fhkVGCK37f7URkoav4Mj7OpLvvr4FLBcvDmQ-sPpTMGCI4Oca3MtN_ypZdWh58Qica96iAT-BBsrILbt86axQs6RaYkCvbPBZeSLHx35p3gMlfBavdJvAvK9yldH5P1riEvB0rgo3rpOL"}
+
+def test_no_user_agent():
+    event = {
+        "body": '{"messages": [], "match_name": "asdad", "match_interests": ["kaka"], "recaptcha_token": "123"}'
+    }
+
+    with pytest.raises(NoUserAgentError):
+        lambda_function.lambda_handler(event, None)
+
+
+def test_malicious_interests():
+    secured_messages = secure_the_messages([["user", "asd\n"]])
+    assert secured_messages[0][1] == "asd. "
